93237c357286a34457d6e310d9899cae41034eb8
Arno Cellarier <bonjour@arnocellarier.fr>
2022-12-17 13:01:18 +0100

    feat: score

---
+++ server/engine/score.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "score.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Usage: %s WORD ANOTHERWORD ...", argv[0]);
+        exit(1);
+    }
+
+    int score = 0;
+    for (int i = 1; i < argc; i++)
+    {
+        score += scoreOfWord(argv[i]);
+    }
+
+    printf("Score: %d", score);
+
+    return 0;
+}
