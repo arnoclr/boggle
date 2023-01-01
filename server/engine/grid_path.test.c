fc39c2c07dd5aafbef94bd34d6af1a05680d70fb
Arno Cellarier <bonjour@arnocellarier.fr>
2023-01-01 17:53:24 +0100

    wip: path detection

--- server/engine/grid_path.test.c
+++ server/engine/grid_path.test.c
@@ -3,19 +3,19 @@
 
 void main()
 {
-    char grid[9] = {'0', '1', '2', '3', '4', '5', '6', '7', '8'};
-
     int siblings[8];
 
-    int siblingOf = 1;
+    // int siblingOf = 8;
 
-    getSiblingsList(siblingOf, grid, &siblings);
+    // char grid2[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    // int path2[16];
 
-    printf("Siblings of %d: %d, %d, %d, %d, %d, %d, %d, %d", siblingOf, siblings[0], siblings[1], siblings[2], siblings[3], siblings[4], siblings[5], siblings[6], siblings[7]);
+    // getSiblingsList(siblingOf, grid2, &siblings, &path2);
 
-    char grid2[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    // printf("Siblings of %d: %d, %d, %d, %d, %d, %d, %d, %d", siblingOf, siblings[0], siblings[1], siblings[2], siblings[3], siblings[4], siblings[5], siblings[6], siblings[7]);
 
-    getSiblingsList(siblingOf, grid2, &siblings);
+    char grid[9] = {'C', 'X', 'X', 'A', 'X', 'X', 'B', 'A', 'C'};
+    char *word = "BAC";
 
-    printf("Siblings of %d: %d, %d, %d, %d, %d, %d, %d, %d", siblingOf, siblings[0], siblings[1], siblings[2], siblings[3], siblings[4], siblings[5], siblings[6], siblings[7]);
+    printf("Word %s is in grid: %d", word, isWordInGrid(word, grid));
 }
