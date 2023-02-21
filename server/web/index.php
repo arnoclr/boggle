64b36cb34725de0cdcd0101e8e70a14ec8859378
Arno Cellarier <bonjour@arnocellarier.fr>
2023-02-21 09:48:18 +0100

    feat: mariadb database with sql init scripts

--- server/web/index.php
+++ server/web/index.php
@@ -29,3 +29,11 @@ switch ($out[0]) {
 
 
 require "views/home.php";
+
+
+// TODO: retirer le test et mettre la connexion à part
+$pdo = new PDO("mysql:host=db;port=3306;dbname=boggle", "boggle", "password");
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$players = $pdo->query("SELECT * FROM players")->fetchAll();
