7955cbfedd81e76e0de48abce5d22dda9bbf1639
Arno Cellarier <bonjour@arnocellarier.fr>
2023-03-07 10:25:13 +0100

    feat: add player record

--- server/web/controllers/createUser.php
+++ server/web/controllers/createUser.php
@@ -3,5 +3,44 @@
 try {
     assertParamsExists(["email"], $_POST);
 } catch (Exception $e) {
-    echo $e->getMessage();
+    respondWithErrorJSON($e->getMessage());
+}
+
+$email = $_POST['email'];
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+    respondWithErrorJSON("L'adresse email n'est pas valide");
+}
+
+try {
+    $playerName = "user-" . random_int(1000, 9999);
+
+    $query = $pdo->prepare("SELECT * FROM players WHERE email = :email");
+    $query->execute([
+        "email" => $email
+    ]);
+
+    $player = $query->fetch();
+
+    if ($player) {
+        respondWithErrorJSON("Un utilisateur avec cette adresse email existe déjà");
+    }
+
+    $query = $pdo->prepare("INSERT INTO players (email, name, createdAt, lastConnection, isPrivateAccount) VALUES (:email, :name, NOW(), NOW(), :isPrivateAccount)");
+    $query->execute([
+        "email" => $email,
+        "name" => $playerName,
+        "isPrivateAccount" => "0"
+    ]);
+
+    $id = $pdo->lastInsertId();
+
+    respondWithSuccessJSON([
+        "id" => $id,
+        "name" => $playerName,
+        "email" => $email
+    ]);
+} catch (Exception $e) {
+    // respondWithErrorJSON("Une erreur est survenue lors de la création de l'utilisateur");
+    respondWithErrorJSON($e->getMessage());
 }
