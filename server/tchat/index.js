93f31651c95467e136ce6215f70f98d3f60c2068
Gabriel François <gfrancois2003@gmail.com>
2023-04-17 18:01:46 +0200

    feat: add io on connection

--- server/tchat/index.js
+++ server/tchat/index.js
@@ -9,8 +9,20 @@ app.get('/', (req, res) => {
   res.send('<p>NodeJS server is working, nothing to see here.</p>');
 });
 
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/test/index.html');
+});
+
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  // Réception d'un message
+  socket.on('message', (message) => {
+    console.log('message received', message);
+
+    // Envoi du message à tous les clients connectés
+    io.emit('message', message);
+  });
 });
 
 server.listen(3000, () => {
