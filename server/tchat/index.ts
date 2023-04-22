f65ae9e0f085b8c560f85bbc625570b6fca6e2ec
Arno Cellarier <bonjour@arnocellarier.fr>
2023-04-22 13:54:45 +0200

    feat: nodemon

--- server/tchat/index.ts
+++ server/tchat/index.ts
@@ -2,7 +2,12 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 
 const httpServer = createServer();
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5500",
+    credentials: true,
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("a user connected");
