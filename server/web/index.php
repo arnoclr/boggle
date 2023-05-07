a915787bc421856ae8a75e4a8b9e48dd5a9fddff
Arno Cellarier <bonjour@arnocellarier.fr>
2023-05-07 19:40:09 +0200

    fix: remove php composer

--- server/web/index.php
+++ server/web/index.php
@@ -5,13 +5,12 @@ session_start();
 header('Access-Control-Allow-Origin: http://localhost:5173');
 header('Access-Control-Allow-Credentials: true');
 
-require "vendor/autoload.php";
 require "env.php";
 
 $pdo = new PDO("mysql:host=" . DB_HOST . ";port=3306;dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$action = $_GET['action'] ?? null;
+$action = $_GET['action'] ?? "";
 
 $controller = "controllers" . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, explode(".", $action)) . ".php";
 
