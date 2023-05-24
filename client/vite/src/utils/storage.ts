dbf9f80f583409a152101d8c690c4809a45ce7db
Arno Cellarier <bonjour@arnocellarier.fr>
2023-05-24 19:04:42 +0200

    feat: save emails in localstorage

--- client/vite/src/utils/storage.ts
+++ client/vite/src/utils/storage.ts
@@ -1,3 +1,7 @@
+function areSame(a: any, b: any): boolean {
+  return JSON.stringify(a) === JSON.stringify(b);
+}
+
 export function getList(listname: string): any[] {
   const list = localStorage.getItem(listname);
   if (list) {
@@ -7,8 +11,22 @@ export function getList(listname: string): any[] {
   }
 }
 
-export function addInList(listname: string, item: any) {
+export function addInList<T>(listname: string, item: T) {
   const list = getList(listname);
   list.push(item);
   localStorage.setItem(listname, JSON.stringify(list));
+}
+
+export function addInListIfNotPresent<T>(listname: string, item: T) {
+  const list = getList(listname);
+  if (!list.some((i) => areSame(i, item))) {
+    list.push(item);
+  }
+  localStorage.setItem(listname, JSON.stringify(list));
+}
+
+export function removeFromList<T>(listname: string, item: T) {
+  const list = getList(listname);
+  const newList = list.filter((i) => !areSame(i, item));
+  localStorage.setItem(listname, JSON.stringify(newList));
 }
