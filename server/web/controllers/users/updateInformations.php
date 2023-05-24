5276e69f6bb7ecb304927abdfccec550fdca2c95
Arno Cellarier <bonjour@arnocellarier.fr>
2023-05-24 17:01:00 +0200

    fix: login flow and boolean insertion in db

--- server/web/controllers/users/updateInformations.php
+++ server/web/controllers/users/updateInformations.php
@@ -11,21 +11,9 @@ function isBoolean($value)
     return is_bool($value) || $value === "true" || $value === "false";
 }
 
-function toBoolean($value)
+function toInt($value)
 {
-    if (is_bool($value)) {
-        return $value;
-    }
-
-    if ($value === "true") {
-        return true;
-    }
-
-    if ($value === "false") {
-        return false;
-    }
-
-    throw new Exception("Invalid boolean value");
+    return $value === "true" ? 1 : 0;
 }
 
 $availableParams = ["name", "profilPicUrl", "isPrivateAccount", "passkey"];
@@ -34,7 +22,7 @@ foreach ($availableParams as $param) {
     if (isset($_POST[$param])) {
         $query = $pdo->prepare("UPDATE players SET $param = :$param WHERE email = :email");
         $query->execute([
-            $param => isBoolean($_POST[$param]) ? toBoolean($_POST[$param]) : $_POST[$param],
+            $param => isBoolean($_POST[$param]) ? toInt($_POST[$param]) : $_POST[$param],
             "email" => $_SESSION['player_email']
         ]);
     }
