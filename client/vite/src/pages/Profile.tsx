72e3caf0bad09584cb2994ce197986043b527d1d
Gabriel François <gfrancois2003@gmail.com>
2023-06-04 17:52:34 +0200

    feat: show games played in profile page

--- client/vite/src/pages/Profile.tsx
+++ client/vite/src/pages/Profile.tsx
@@ -10,7 +10,15 @@ export default function Profile() {
     isPublicAccount: boolean,
     totalGames: number,
     totalScore: number,
-    totalWordsFound: number
+    totalWordsFound: number,
+    games: Array<{
+      publicId: string,
+      startedAt: string,
+      totalWordsFound: string,
+      totalScore: number,
+      bestWord: string | null,
+      bestWordScore: number
+    }>
   } | null>(null);
   
   async function fetchProfileData(userName: string): Promise<void> {
@@ -20,12 +28,14 @@ export default function Profile() {
         "profil.getUserStats",
         toMap({ userName })
       );
-      const isPublicAccout = response.data.isPublic;
+      console.log(response);
+      const isPublicAccount = response.data.isPublic;
       const totalGames = response.data.totalGames;
       const totalScore = response.data.totalScore;
       const totalWordsFound = response.data.totalWordsFound;
+      const games = response.data.games;
 
-      setProfileData({ isPublicAccount: isPublicAccout, totalGames: totalGames, totalScore: totalScore, totalWordsFound: totalWordsFound });
+      setProfileData({ isPublicAccount: isPublicAccount, totalGames: totalGames, totalScore: totalScore, totalWordsFound: totalWordsFound, games: games });
     } catch (e) {
       const { message, status } = e as ErrorWithStatus;
       if (status === "user_not_found") {
@@ -45,16 +55,34 @@ export default function Profile() {
       <h1>Profile</h1>
       <p>@{username}</p>
       {isLoading && <p>Loading...</p>}
-      {profileData.isPublicAccount ? (
+      {profileData && profileData.isPublicAccount ? (
         <div>
           <p>Nombre de parties jouées: {profileData.totalGames}</p>
           <p>Nombre de points: {profileData.totalScore}</p>
           <p>Nombre de mots trouvés: {profileData.totalWordsFound}</p>
+          <h2>Liste des parties jouées :</h2>
+          <ul>
+            {profileData.games.map((game) => (
+              <li key={game.publicId}>
+                <p>Started at: {game.startedAt}</p>
+                <p>Nombre de mots trouvés: {game.totalWordsFound}</p>
+                <p>Score total: {game.totalScore}</p>
+                <p>
+                  Meilleur mot trouvé:{" "}
+                  {game.bestWord ? (
+                    game.bestWord
+                  ) : (
+                    <span style={{ fontStyle: "italic", color: "gray" }}>Aucun</span>
+                  )}
+                </p>
+                <a href={`/g/${game.publicId}`}>Voir la partie</a>
+              </li>
+            ))}
+          </ul>
         </div>
       ) : (
         <p>Ce compte est privé.</p>
-        )
-      }
+      )}
     </div>
   );
 }
