b00ce6fbd8f611223fd88cd49478ff6dea33e82a
Gabriel François <gfrancois2003@gmail.com>
2023-06-04 16:30:02 +0200

    feat: add API getUserStats

---
+++ server/web/controllers/profil/getUserStats.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+- Affichage que si la personne a un compte publique (verification en faisant la condition dans la requete SQL directement)
+- Affichage infos sur joueur, nom, stats générales ...
+- Affichage de l'historique avec possibilité de cliquer sur une partie et sa ramène vers /g/ID partie. La aussi il faut quelques stats générales sur la partie, donc meilleur mot trouvé par le joueur, sa position dans le classement, nombre de mots trouvés, score, joueurs présents, date ...
+- En bonus : affichage des PP et possibilité de les choisir parmis une liste
+*/
+
+$userName = $_POST['userName'] ?? null;
+echo $userName;
+
+if (!$userName) {
+    // TODO: Récupérer le nom de l'utilisateur connecté
+}
+
+$stmt = $pdo->prepare("SELECT * FROM players WHERE name = :userName");
+$stmt->execute([
+    "userName" => $userName
+]);
+
+$user = $stmt->fetch();
+
+if (!$user) {
+    respondWithErrorJSON("user_not_found");
+}
+
+$stmt = $pdo->prepare("SELECT COUNT(*) AS totalGames FROM gamesplayers WHERE idPlayer = :idPlayer");
+$stmt->execute([
+    "idPlayer" => $user->idPlayer
+]);
+
+$totalGames = $stmt->fetch();
+
+$stmt = $pdo->prepare("
+    SELECT 
+        SUM(score) AS totalScore,
+        COUNT(*) AS totalWordsFound
+    FROM 
+        wordsfound 
+    WHERE 
+        idPlayer = :idPlayer
+");
+$stmt->execute([
+    "idPlayer" => $user->idPlayer
+]);
+
+$stats = $stmt->fetch();
+
+respondWithSuccessJSON([
+    "userName" => $user->name,
+    "totalGames" => $totalGames->totalGames ?? 0,
+    "totalScore" => $stats->totalScore ?? 0,
+    "totalWordsFound" => $stats->totalWordsFound ?? 0
+]);
